8d997cfb293db9422873cce5c45c875276669006
i-saw-sparks <andresmolinaceti@gmail.com>
2019-01-31 22:14:43 -0600

    Primer paso a elegir personajes

--- Assets/Scripts/Network/JugadorNet.cs
+++ Assets/Scripts/Network/JugadorNet.cs
@@ -6,7 +6,13 @@ using UnityEngine.Networking;
 public class JugadorNet : NetworkBehaviour
 {
 
+    public GameObject Mago;
+    public GameObject Herrero;
+    public GameObject Princesa;
+    public GameObject Caballero;
     public GameObject Cierra;
+    public GameObject Capsula;
+    public int opcion;
 
     // Start is called before the first frame update
     void Start()
@@ -18,13 +24,37 @@ public class JugadorNet : NetworkBehaviour
         }
 
         Cierra = GameObject.Find("Carga");
-
+        Capsula = GameObject.Find("Capsula");
+        opcion = (int) Capsula.transform.position.x;
         //lista de condiciones para ver que personaje es
         Cierra.SetActive(false);
-        CmdSpawnMago();
+
+        switch (opcion)
+        {
+            case 1:
+                CmdSpawnPrin();
+                break;
+
+            case 2:
+                CmdSpawnMago();
+                break;
+
+            case 3:
+                CmdSpawnCaballero();
+                break;
+
+            case 4:
+                CmdSpawnHerrero();
+                break;
+
+            default:
+                break;
+        }
+
+       
     }
 
-    public GameObject Personaje;
+
     // Update is called once per frame
     void Update()
     {
@@ -33,10 +63,32 @@ public class JugadorNet : NetworkBehaviour
 
     [Command]
     void CmdSpawnMago(){
-        GameObject jugador = Instantiate(Personaje);
+        GameObject jugador = Instantiate(Mago);
 
         NetworkServer.SpawnWithClientAuthority(jugador, connectionToClient);
     }
 
+    [Command]
+    void CmdSpawnHerrero()
+    {
+        GameObject jugador = Instantiate(Herrero);
 
+        NetworkServer.SpawnWithClientAuthority(jugador, connectionToClient);
+    }
+
+    [Command]
+    void CmdSpawnPrin()
+    {
+        GameObject jugador = Instantiate(Princesa);
+
+        NetworkServer.SpawnWithClientAuthority(jugador, connectionToClient);
+    }
+
+    [Command]
+    void CmdSpawnCaballero()
+    {
+        GameObject jugador = Instantiate(Caballero);
+
+        NetworkServer.SpawnWithClientAuthority(jugador, connectionToClient);
+    }
 }
