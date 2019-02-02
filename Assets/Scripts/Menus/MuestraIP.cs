639a4b0f70af4b79dfab46bb12e3fa984f752fb5
i-saw-sparks <andresmolinaceti@gmail.com>
2019-02-01 23:56:55 -0600

    menu de conexion paso 1

--- Assets/Scripts/Menus/MuestraIP.cs
+++ Assets/Scripts/Menus/MuestraIP.cs
@@ -1,19 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
 
-public class MuestraIP : MonoBehaviour
+public class MuestraIP : NetworkBehaviour
 {
+    public Text ip;
+    
     // Start is called before the first frame update
     void Start()
     {
-
-
+             
+        ip = GetComponent<Text>();
+        ip.text = LocalIPAddress();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public static string LocalIPAddress()
+    {
+        IPHostEntry host;
+        string localIP = "0.0.0.0";
+        host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (IPAddress ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                localIP = ip.ToString();
+                break;
+            }
+        }
+        return localIP;
+
     }
 }
