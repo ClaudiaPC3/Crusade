afa4f61432b22bfb2d847430cd1dcc2eeb732877
i-saw-sparks <andresmolinaceti@gmail.com>
2019-02-02 18:09:50 -0600

    struct de casilla

---
+++ Assets/Prefabs/Mapa/Estructuras/Generacion.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    struct casilla
+    {
+        public bool taken;
+        public int x, y, id;
+        public casilla(int x, int y, int id, bool taken)
+        {
+            this.x = x;
+            this.y = y;
+            this.id = id;
+            this.taken = taken;
+        }
+    }
+
+    casilla[,] mapa = new casilla [19,8];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
