f11b7e3544b0b118a9b98074f9d8cd9b9a8abb0a
i-saw-sparks <andresmolinaceti@gmail.com>
2019-02-22 22:55:28 -0600

    Organizacion de archivos

---
+++ Assets/Scripts/HUD/Monedas/MonControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MonControl : MonoBehaviour
+{
+    public Text cantmon;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cantmon.text = GlobalData.Monedas.ToString();
+    }
+
+    public void AumentaMon()
+    {
+        GlobalData.Monedas++;
+    }
+}
