06e720bc505e1f6844b34fe8e54dd7eafa8dca57
i-saw-sparks <andresmolinaceti@gmail.com>
2019-02-24 22:04:59 -0600

    reloj paso 2

--- Assets/Scripts/HUD/Reloj/Reloj.cs
+++ Assets/Scripts/HUD/Reloj/Reloj.cs
@@ -6,10 +6,10 @@ using UnityEngine.UI;
 public class Reloj : MonoBehaviour
 {
     public Text reloj;
-    int segs = 30;
-    int min = 0;
-    bool pre = false;
-    
+    int segs = 10;
+    int mins = 0;
+    bool pre = true;
+    float counter = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +19,31 @@ public class Reloj : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (GlobalData.Desb == true)
+        {
+            counter += Time.deltaTime;
+            reloj.fontSize = 90;
+            if (counter >= 1f && pre == true)
+            {
+                counter = 0;
+                segs--;
+                if (segs == 0)
+                {
+                    pre = false;
+                }
+            }
+            if (counter >= 1f && pre == false)
+            {
+                counter = 0;
+                segs++;
+                if (segs > 59)
+                {
+                    mins++;
+                    segs = 0;
+                }
+            }
+            reloj.text = mins + ":" + segs;
+        }
         
     }
 }
