eaeb833d6d3d72378015301f5aaabb0132bd3727
i-saw-sparks <andresmolinaceti@gmail.com>
2019-03-09 15:52:14 -0600

    chicle paso 3

--- Assets/Scripts/Habilidades/Princesa/Chicle.cs
+++ Assets/Scripts/Habilidades/Princesa/Chicle.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class Chicle : MonoBehaviour
 {
     public bool Coll = false;
+    public Sprite chicle;
     
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Coll = true;
+        if(collision.transform.gameObject.tag == "Pared")
+        {
+            collision.transform.gameObject.GetComponent<SpriteRenderer>().sprite = chicle;
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
