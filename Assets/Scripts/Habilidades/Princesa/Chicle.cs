8e8db4a67bbf8e8b54b3ca0b14b810badc6a7483
i-saw-sparks <andresmolinaceti@gmail.com>
2019-03-09 16:15:22 -0600

    Autodestructor de chicles

--- Assets/Scripts/Habilidades/Princesa/Chicle.cs
+++ Assets/Scripts/Habilidades/Princesa/Chicle.cs
@@ -4,23 +4,36 @@ using UnityEngine;
 
 public class Chicle : MonoBehaviour
 {
-    public bool cont = false;
+    private bool cont = false, coll = false;
+    private float counter = 0;
     public Sprite chicle;
+    public GameObject me;
 
-    
+    private void Update()
+    {
+        if (!coll)
+        {
+            counter += Time.deltaTime;
+            if(counter >= 1)
+            {
+                Destroy(me);
+            }
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         
         if(collision.transform.gameObject.tag == "Pared"&&!cont)
         {
             collision.transform.gameObject.GetComponent<SpriteRenderer>().sprite = chicle;
-            cont = true;
+            coll = true;
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        
+        Destroy(me);
     }
 
     
