1e68f1a0621be809820fedbedccace5f94bf484f
i-saw-sparks <andresmolinaceti@gmail.com>
2019-03-15 19:31:44 -0600

    Escalera / activacion previa a toque de pared es imposible

--- Assets/Scripts/Habilidades/Princesa/Escalera.cs
+++ Assets/Scripts/Habilidades/Princesa/Escalera.cs
@@ -4,40 +4,55 @@ using UnityEngine;
 
 public class Escalera : MonoBehaviour
 {
-    public bool active = false, procdeac = false;
+    public bool active = false;
     private float counter = 0;
-    
+    private bool touchPar = false;
     public GameObject me;
 
     public void Update()
     {
-        if (procdeac)
+        if (active)
         {
             counter += Time.deltaTime;
             if (counter >= 0.1)
             {
                 me.GetComponent<PolygonCollider2D>().isTrigger = false;
                 active = false;
-                procdeac = false;
                 counter = 0;
             }
         }
     }
 
+    
+
     // Update is called once per frame
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.gameObject.tag == "Pared")
+        {
+            touchPar = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.transform.gameObject.tag == "Pared")
+        {
+            touchPar = false;
+        }
+    }
+
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.transform.gameObject.tag == "Pared" && active)
+        if(active && touchPar)
         {
-            
-            me.GetComponent<PolygonCollider2D>().isTrigger = true;            
-            
+            me.GetComponent<PolygonCollider2D>().isTrigger = true;
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        procdeac = false;
+        touchPar = false;        
     }
 
     private void OnTriggerStay2D(Collider2D collision)
@@ -46,10 +61,5 @@ public class Escalera : MonoBehaviour
         {
             counter = 0;
         }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        procdeac = true; 
     }
 }
